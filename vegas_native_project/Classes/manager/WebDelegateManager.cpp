a634d9c6be5d2271477b55776264ddaa992da4d7
directxm <53228376@qq.com>
2017-01-09 11:15:27 +0800

    network uncompleted

--- vegas_native_project/Classes/manager/WebDelegateManager.cpp
+++ vegas_native_project/Classes/manager/WebDelegateManager.cpp
@@ -4,13 +4,98 @@ USING_NS_CC;
 
 WebSocketDelegate::WebSocketDelegate()
 {
-
+	connectRetryTimes = 0;
 }
 
 WebSocketDelegate::~WebSocketDelegate()
 {
 
 }
+
+
+bool WebSocketDelegate::hasNetwork()
+{
+	return true;
+}
+
+// app当前是否有权限连接网络
+bool WebSocketDelegate::checkPermission()
+{
+	return true;
+}
+
+std::string WebSocketDelegate::getIp()
+{
+	return "";
+}
+
+int WebSocketDelegate::getPort()
+{
+	return 0;
+}
+
+bool WebSocketDelegate::resetNetwork()
+{
+	disconnect();
+	clear();
+
+	return true;
+}
+
+bool WebSocketDelegate::connect()
+{
+	// 网络是否正常
+	if (hasNetwork() == false || checkPermission() == false)
+	{
+		onError(ErrorCode::NO_NET);
+		return false;
+	}
+
+	// 是否正在连接
+	if (false)
+	{
+		return false;
+	}
+
+	// 重连次数过多
+	++connectRetryTimes;
+	if (connectRetryTimes >= 3)
+	{
+		onError(ErrorCode::NO_NET);
+		return false;
+	}
+
+	if (doConnect())
+	{
+		return true;
+	}
+	else
+	{
+		onError(ErrorCode::UNKNOWN);
+		return false;
+	}
+}
+
+bool WebSocketDelegate::disconnect()
+{
+	return true;
+}
+
+bool WebSocketDelegate::isConnect()
+{
+	return true;
+}
+
+bool WebSocketDelegate::send()
+{
+	return true;
+}
+
+void WebSocketDelegate::clear()
+{
+
+}
+
 
 
 void WebSocketDelegate::onOpen(cocos2d::network::WebSocket* ws)
@@ -20,6 +105,10 @@ void WebSocketDelegate::onOpen(cocos2d::network::WebSocket* ws)
 
 void WebSocketDelegate::onMessage(cocos2d::network::WebSocket* ws, const cocos2d::network::WebSocket::Data& data)
 {
+	if (!data.isBinary)
+	{
+	}
+
 	throw std::logic_error("The method or operation is not implemented.");
 }
 
@@ -33,3 +122,12 @@ void WebSocketDelegate::onError(cocos2d::network::WebSocket* ws, const cocos2d::
 	throw std::logic_error("The method or operation is not implemented.");
 }
 
+void WebSocketDelegate::onError(const ErrorCode& error)
+{
+
+}
+
+bool WebSocketDelegate::doConnect()
+{
+	return true;
+}
--- vegas_native_project/Classes/manager/WebDelegateManager.h
+++ vegas_native_project/Classes/manager/WebDelegateManager.h
@@ -7,9 +7,38 @@
 class WebSocketDelegate : public cocos2d::network::WebSocket::Delegate
 {
 public:
+	enum class ErrorCode
+	{
+		TIME_OUT,           /** &lt; value 0 */
+		CONNECTION_FAILURE, /** &lt; value 1 */
+		UNKNOWN,            /** &lt; value 2 */
+		NO_NET,           /** &lt; value 3 */
+	};
+public:
 	WebSocketDelegate();
 	~WebSocketDelegate();
 
+
+	//void httpRequest();
+
+	bool hasNetwork();
+	// app当前是否有权限连接网络
+	bool checkPermission();
+
+	std::string getIp();
+	int getPort();
+
+	bool resetNetwork();
+
+	bool connect();
+	bool disconnect();
+	bool isConnect();
+
+	bool send();
+
+	void clear();
+
+protected:
 	virtual void onOpen(cocos2d::network::WebSocket* ws) override;
 
 	virtual void onMessage(cocos2d::network::WebSocket* ws, const cocos2d::network::WebSocket::Data& data) override;
@@ -18,12 +47,16 @@ public:
 
 	virtual void onError(cocos2d::network::WebSocket* ws, const cocos2d::network::WebSocket::ErrorCode& error) override;
 
+	virtual void onError(const ErrorCode& error);
 
-	void httpRequest();
-
+	virtual bool doConnect();
 protected:
 	//void onHttpRequest(CCHttpClient* client, CCHttpResponse* response);
 
+protected:
+	int connectRetryTimes;
+
+	cocos2d::network::WebSocket* citySocket;
 private:
 };
 
