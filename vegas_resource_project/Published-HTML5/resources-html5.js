a4a3dfd5246f78e54f3b6dd1cbb2cc20bf4cc430
bianxueming <bianxueming@pwrd.com>
2017-01-25 15:02:17 +0800

    add resource project

---
+++ vegas_resource_project/Published-HTML5/resources-html5.js
@@ -0,0 +1,11 @@
+var ccb_resources = [
+    {type:'image', src:"res/clickme-down.png"},
+    {type:'image', src:"res/ccbResources/ccbParticleFire.png"},
+    {type:'image', src:"res/clickme.png"},
+    {type:'ccbi', src:"res/ccbi/FruitMachineScene.ccbi"},
+    {type:'image', src:"res/ccbResources/ccbParticleSnow.png"},
+    {type:'image', src:"res/ccbResources/ccbParticleSmoke.png"},
+    {type:'image', src:"res/ccbResources/ccbParticleStars.png"},
+    {type:'plist', src:"res/ccbResources/ccbDefaultImages.plist"},
+    {type:'image', src:"res/ccbResources/ccbDefaultImages.png"},
+];
